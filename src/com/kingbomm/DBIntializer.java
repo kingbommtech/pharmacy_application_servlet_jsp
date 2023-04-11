7975996c07b68a93ffb3696b52a30190fc22721c
kingbommtech <kinshukmaity555@gmail.com>
2023-04-11 14:36:01 +0530

    pharmacy

---
+++ src/com/kingbomm/DBIntializer.java
@@ -0,0 +1,8 @@
+package com.kingbomm;
+
+public interface DBIntializer {
+String DRIVER="oracle.jdbc.driver.OracleDriver";
+String CON_STRING="jdbc:oracle:thin:@localhost:1521:ORCL";
+String USERNAME="KINSHUK";
+String PASSWORD="MANTU";
+}
